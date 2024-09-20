b1d9b58dfee871a534790e17742f77b59fbb1d1f
Jirasit <48820052+TONG2K@users.noreply.github.com>
2024-09-21 00:37:57 +0700

    Tong: update navbar structure file

--- srcs/react-app/src/App.js
+++ srcs/react-app/src/App.js
@@ -1,31 +1,25 @@
+// import logo from './logo.svg';
 import './App.css';
-import { FaSignOutAlt, FaBell, FaUserCircle } from 'react-icons/fa';
-import logo from './assets/MUT_LOGO_crop.png';
+import Navbar from './components/navbar/navbar';
 
 function App() {
 	return (
 		<div className="App">
-			<nav className="navbar">
-				<div className="navbar-left">
-					<a href="#home" className="logo">
-						<img src={logo} alt="MUT Logo" />
-					</a>
-
-					<ul className="navbar-links">
-						<li><a href="#reserve">Reserve</a></li>
-						<li><a href="#history">History</a></li>
-						<li><a href="#management">Management</a></li>
-						<li><a href="#master">Master</a></li>
-						<li><a href="#report">Report</a></li>
-					</ul>
-				</div>
-
-				<div className="navbar-icons">
-					<a href="#signout" className="navbar-icon"><FaSignOutAlt /></a>
-					<a href="#notifications" className="navbar-icon"><FaBell /></a>
-					<a href="#profile" className="navbar-icon"><FaUserCircle /></a>
-				</div>
-			</nav>
+			{/* <header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header> */}
+			<Navbar />
 		</div>
 	);
 }
---
+++ srcs/react-app/src/components/navbar/navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import '../styles/styles.css';
+import { FaSignOutAlt, FaBell, FaUserCircle } from 'react-icons/fa';
+import logo from '../../assets/MUT_LOGO_crop.png';
+
+function Navbar() {
+	return (
+		<nav className="navbar">
+			<div className="navbar-left">
+				<a href="#home" className="logo">
+					<img src={logo} alt="MUT Logo" />
+				</a>
+
+				<ul className="navbar-links">
+					{['Reserve', 'History', 'Management', 'Master', 'Report'].map(item => (
+						<li key={item}><a href={`#${item.toLowerCase()}`}>{item}</a></li>
+					))}
+				</ul>
+			</div>
+
+			<div className="navbar-icons">
+				{[
+					{ href: '#signout', icon: <FaSignOutAlt /> },
+					{ href: '#notifications', icon: <FaBell /> },
+					{ href: '#profile', icon: <FaUserCircle /> },
+				].map(({ href, icon }) => (
+					<a key={href} href={href} className="navbar-icon">{icon}</a>
+				))}
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
