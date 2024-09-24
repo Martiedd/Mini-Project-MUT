9dfa15c559a54d910b124f715511d486948d3350
Chitsanupong13 <6511130043@mut.ac.th>
2024-09-24 21:11:36 +0700

    Mix: init events

--- srcs/react-app/src/components/management/manage.js
+++ srcs/react-app/src/components/management/manage.js
@@ -1,96 +1,152 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/master.css';
-import add from '../../assets/Adds.png';
-import deletes from '../../assets/Delete.png';
-import edits from '../../assets/Edits.png';
-import search from '../../assets/Search.png';
+import React, { useState, useEffect } from "react";
+import "../styles/master.css";
+import add from "../../assets/Adds.png";
+import deletes from "../../assets/Delete.png";
+import edits from "../../assets/Edits.png";
+import search from "../../assets/Search.png";
 
 function Manage() {
+  const [page_id, setPageId] = useState(0);
+  const [Is_underlinedId, setUnderlinedId] = useState(null);
+  const [columns, setColumns] = useState([]); // เก็บข้อมูลคอลัมน์ใน <thead>
+  const [Add_Index, setAdd] = useState(0);
 
-	const [Is_underlinedId, setUnderlinedId] = useState(null);
-	const [columns, setColumns] = useState([]); // เก็บข้อมูลคอลัมน์ใน <thead>
+  const Add_Close = () => {
+    setAdd(0);
+  };
 
-	const Page_Click = (id) => {
-		setUnderlinedId(id); // ตั้งค่า id ของ <p> ที่ถูกคลิก
+  const Add_Open = () => {
+    if (parseInt(page_id) === 1) setAdd(1);
+    else if (parseInt(page_id) === 2) setAdd(2);
+  };
 
-		if (id === 1) {
-			// เพิ่ม <th> Columns เมื่อคลิก User
-			const newColumns = ['รหัสผู้ใช้', 'ชื่อผู้ใช้', 'นามสกุล', 'แผนก', 'ตำแหน่ง', 'สถานะ', 'คะแนนผู้ใช้'];
-			setColumns(newColumns);
-		}
+  const Page_Click = (id) => {
+    setUnderlinedId(id); // ตั้งค่า id ของ <p> ที่ถูกคลิก
+    setPageId(id);
+    if (id === 1) {
+      // เพิ่ม <th> Columns เมื่อคลิก User
+      const newColumns = [
+        "รหัสผู้ใช้",
+        "ชื่อผู้ใช้",
+        "นามสกุล",
+        "แผนก",
+        "ตำแหน่ง",
+        "สถานะ",
+        "คะแนนผู้ใช้"
+      ];
+      setColumns(newColumns);
+    } else {
+      // เพิ่ม <th> Columns เมื่อคลิก Room
+      const newColumns = [
+        "รหัสห้อง",
+        "ชื่อห้อง",
+        "รหัสตึก",
+        "ชั้น",
+        "ระดับห้อง",
+        "สถานะ",
+        "ความจุ"
+      ];
+      setColumns(newColumns);
+    }
+  };
+  // เรียกใช้ Func Page_Click(1) ครั้งแรกเมื่อหน้าเว็บโหลด
+  useEffect(() => {
+    Page_Click(1); // เรียกฟังก์ชันเมื่อโหลดครั้งแรก
+  }, []); // [] หมายถึงเรียกใช้เมื่อคอมโพเนนต์ถูก mount ครั้งแรก
 
-		else {
-			// เพิ่ม <th> Columns เมื่อคลิก Room
-			const newColumns = ['รหัสห้อง', 'ชื่อห้อง', 'รหัสตึก', 'ชั้น', 'ระดับห้อง', 'สถานะ', 'ความจุ'];
-			setColumns(newColumns);
-		}
-	};
+  return (
+    <div className="container">
+      <div className="selection-zone">
+        <p
+          className="user"
+          onClick={() => Page_Click(1)}
+          style={{
+            textDecoration: Is_underlinedId === 1 ? "underline" : "none",
+            cursor: "pointer"
+          }}
+        >
+          User
+        </p>
 
-	// เรียกใช้ Func Page_Click(1) ครั้งแรกเมื่อหน้าเว็บโหลด
-	useEffect(() => {
-		Page_Click(1); // เรียกฟังก์ชันเมื่อโหลดครั้งแรก
-	}, []); // [] หมายถึงเรียกใช้เมื่อคอมโพเนนต์ถูก mount ครั้งแรก
+        <p
+          className="room"
+          onClick={() => Page_Click(2)}
+          style={{
+            textDecoration: Is_underlinedId === 2 ? "underline" : "none",
+            cursor: "pointer"
+          }}
+        >
+          Room
+        </p>
+      </div>
 
-	return (
-		<div className='container'>
+      <div className="event-zone">
+        <button type="submit" onClick={() => Add_Open()}>
+          <img src={add} alt="add" className="add-data" />
+          Add
+        </button>
 
-			<div className='selection-zone'>
+        <button type="submit">
+          <img src={edits} alt="edits" className="edits-data" />
+          Edits
+        </button>
 
-				<p className="user"
-					onClick={() => Page_Click(1)}
-					style={{
-						textDecoration: Is_underlinedId === 1 ? 'underline' : 'none',
-						cursor: 'pointer'
-					}}
-				>User</p>
+        <button type="submit">
+          <img src={deletes} alt="delete" className="delete-data" />
+          Delete
+        </button>
 
-				<p className="room"
-					onClick={() => Page_Click(2)}
-					style={{
-						textDecoration: Is_underlinedId === 2 ? 'underline' : 'none',
-						cursor: 'pointer'
-					}}
-				>Room</p>
+        <div class="search-container">
+          <input
+            className="input-text"
+            type="text"
+            placeholder="Search.."
+            name="search"
+          ></input>
+          <button className="input-pic" type="input-pic">
+            <img className="search-data" src={search} alt="search" />
+          </button>
+        </div>
+      </div>
 
-			</div>
+      <div className="table-zone">
+        <table>
+          <thead>
+            <tr>
+              {/* ใช้ map เพื่อสร้าง <th> ทั้ง 7 คอลัมน์เมื่อมีการคลิก */}
+              {columns.map((column, index) => (
+                <th key={index}>{column}</th>
+              ))}
+            </tr>
+          </thead>
+        </table>
+      </div>
 
-			<div className='event-zone'>
+      {Add_Index === 1 && (
+        <div className="popup">
+          <div className="popup-inner">
+            <button onClick={Add_Close} className="close-popup">
+              Close
+            </button>
+          </div>
+        </div>
+      )}
 
-				<button type='submit'>
-					<img src={add} alt='add' className='add-data' />
-					Add
-				</button>
-
-				<button type='submit'>
-					<img src={edits} alt='edits' className='edits-data' />
-					Edits
-				</button>
-
-				<button type='submit'>
-					<img src={deletes} alt='delete' className='delete-data' />
-					Delete
-				</button>
-
-				<div class="search-container">
-					<input className='input-text' type="text" placeholder="Search.." name="search"></input>
-					<button className='input-pic' type="input-pic"><img className='search-data' src={search} alt='search' /></button>
-				</div>
-			</div>
-
-			<div className='table-zone'>
-				<table>
-					<thead>
-						<tr>
-							{/* ใช้ map เพื่อสร้าง <th> ทั้ง 7 คอลัมน์เมื่อมีการคลิก */}
-							{columns.map((column, index) => (
-								<th key={index}>{column}</th>
-							))}
-						</tr>
-					</thead>
-				</table>
-			</div>
-		</div>
-	);
-};
+      {Add_Index === 2 && (
+        <div className="popup">
+          <div className="popup-inner">
+            <h2>Form 2</h2>
+            <form>
+              <input type="text" placeholder="Address" />
+            </form>
+            <button onClick={Add_Close} className="close-popup">
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default Manage;
